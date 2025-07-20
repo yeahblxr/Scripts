0cc2788d3480f4ffdf77465fe783c0e175736557
yeahblxr <yeahblxr@gmail.com>
2025-07-20 12:28:17 -0500

    Update Midnightbetatest.lua

--- Midnightbetatest.lua
+++ Midnightbetatest.lua
@@ -38,7 +38,7 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
-local Tab = Window:CreateTab("Home", "house")
+local Tab = Window:CreateTab("Home", 4483362458)
 
 local Button = Tab:CreateButton({
    Name = "Discord",
