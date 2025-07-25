7d5ef85443869d3a9cb1c949c724b175d9bae439
yeahblxr <yeahblxr@gmail.com>
2025-07-25 18:19:05 -0500

    Update ‎Midnight Hub.lua

--- ‎Midnight Hub.lua
+++ ‎Midnight Hub.lua
@@ -1,4 +1,5 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "Script Changed",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
